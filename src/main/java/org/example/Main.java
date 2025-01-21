af71778dbba8b9f520c3e11e5371966e92b26e49
Ramazan Abdullayev <r.abdullayev.dev@gmail.com>
2025-01-21 17:31:18 +0400

    The circle's volume, area and circumference

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,46 @@
 package org.example;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-        double width = 0;
-        double height = 0;
-        double area = 0;
+//        System.out.println(Math.PI);
+//        System.out.println(Math.E);
+//        System.out.println(Math.TAU+"\n");
+//
+//        double result;
+//
+//        result = (int) Math.pow(2, 5);
+//        System.out.println(result);
+//        result = Math.sqrt(4);
+//        System.out.println(result);
+//        result = Math.abs(-5);
+//        System.out.println(result);
+//        result = Math.round(6.4);
+//        System.out.println(result);
+//        result = Math.ceil(6.2);
+//        System.out.println(result);
+//        result = Math.floor(6.7);
+//        System.out.println(result);
+//        result = Math.max(8, 9);
+//        System.out.println(result);
+//        result = Math.min(8, 9);
+//        System.out.println(result);
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the width of rectangle: ");
-        width = scanner.nextDouble();
-        System.out.println("Enter the heigth of rectangle: ");
-        height = scanner.nextDouble();
+        double radius;
+        double circumference;
+        double area;
+        double volume;
 
-        area = width * height;
-        System.out.println("The area of rectangle is " + area + "cm²");
+        System.out.print("Enter the radius of circle: ");
+        radius = scanner.nextDouble();
 
-        scanner.close();
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("The circumference of the circle is %.1fcm\nThe area of the circle is %.1fcm²\nThe volume of the circle is %.1fcm³\n", circumference, area, volume);
     }
 }
