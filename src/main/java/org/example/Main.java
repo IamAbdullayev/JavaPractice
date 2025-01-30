895dd60bdb8e1171ed4a0e80d615b7e4c9ec989b
Ramazan <ramazan.abdullayev@bsu.edu.az>
2025-01-30 22:10:10 +0400

    Quiz Game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,83 +3,52 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Random randim = new Random();
+        // The Quiz Game
+
+        String[] quetions = {"What is the main function of a router?",
+                             "Which part of the computer considered with brain?",
+                             "What year was Facebook launched?",
+                             "Who known as the father of computer?",
+                             "What was the first programming language?"};
+
+        String[][] options = {{"1. Storing files", "2. Encrypting data", "3. Directing internet traffic", "4. Managing passwords"},
+                              {"1. HDD", "2. CPU", "3. RAM", "4. GPU"},
+                              {"1. 2004", "2. 2001", "3. 2009", "4. 2024"},
+                              {"1. Steve Jobs", "2. Bill Gates", "3. Alan Turing", "4. Charles Babbage"},
+                              {"1. COBOL", "2. C", "3. Fortran", "4. Assembly"}};
+
+        int[] answers = {3, 2, 1, 4, 3};
+        int score = 0;
+        int guess;
+
         Scanner scanner = new Scanner(System.in);
 
-        int numOfDice;
-        int total = 0;
+        System.out.println("==========================");
+        System.out.println("Welcome to the Quiz Game!");
+        System.out.println("==========================");
 
-        System.out.print("Enter the # of dice to roll: ");
-        numOfDice = scanner.nextInt();
-
-        if(numOfDice > 0) {
-            for (int i = 0; i < numOfDice; ++i) {
-                int roll = randim.nextInt(1, 7);
-                System.out.println("You rolled: " + roll);
-                printDie(roll);
-                total += roll;
+        for (int i = 0; i < quetions.length; ++i) {
+            System.out.println(quetions[i]);
+            for (int j = 0; j < options[i].length; ++j) {
+                System.out.println(options[i][j]);
             }
-            System.out.println("Total: " + total);
-        } else {
-            System.out.println("# of dice must be greater than 0.");
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            if (guess == answers[i]) {
+                System.out.println("=======");
+                System.out.println("CORRECT");
+                System.out.println("=======");
+                score++;
+            } else {
+                System.out.println("=====");
+                System.out.println("WRONG");
+                System.out.println("=====");
+            }
         }
-    }
-    static void printDie(int roll) {
+        System.out.println("Your final score is: " + score + " out of " + quetions.length);
 
-        String dice1 = """
-                 ---------
-                |         |
-                |    ●    |
-                |         |
-                 ---------
-                """;
-        String dice2 = """
-                 ---------
-                |      ●  |
-                |         |
-                |  ●      |
-                 ---------
-                """;
-        String dice3 = """
-                 ---------
-                |      ●  |
-                |    ●    |
-                |  ●      |
-                 ---------
-                """;
-        String dice4 = """
-                 ---------
-                | ●     ● |
-                |         |
-                | ●     ● |
-                 ---------
-                """;
-        String dice5 = """
-                 ---------
-                | ●     ● |
-                |    ●    |
-                | ●     ● |
-                 ---------
-                """;
-        String dice6 = """
-                 ---------
-                |  ●   ●  |
-                |  ●   ●  |
-                |  ●   ●  |
-                 ---------
-                """;
-
-        switch (roll) {
-            case 1 -> System.out.println(dice1);
-            case 2 -> System.out.println(dice2);
-            case 3 -> System.out.println(dice3);
-            case 4 -> System.out.println(dice4);
-            case 5 -> System.out.println(dice5);
-            case 6 -> System.out.println(dice6);
-            default -> System.out.println("Invalid roll.");
-        }
+        scanner.close();
     }
 }
