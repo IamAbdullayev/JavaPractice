d0869a8f8a8e5ce8c724c3a994157320cf9dee8a
Ramazan Abdullayev <r.abdullayev.dev@gmail.com>
2025-02-05 21:42:14 +0400

    Music player

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,60 @@
 package org.example;
-import java.io.*;
+import javax.sound.sampled.*;
+import javax.xml.stream.events.StartDocument;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        String pathFile = "C:\\Users\\ramaz\\Downloads\\Billie Eilish - BIRDS OF A FEATHER (from Saturday Night Live, 2024).wav";
+        File file = new File(pathFile);
 
-        String filePath = "C:\\Users\\ramaz\\Desktop\\JavaEE_copy.docx";
+        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            Scanner in = new Scanner(System.in)) {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            System.out.println("File exist! ;D\n");
+            System.out.println("AAA");
 
-            String line;
-            while((line = reader.readLine()) != null) {
-                System.out.println(line);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+
+            String response = "";
+
+            while (!response.equals("Q")) {
+                System.out.println("Play = P");
+                System.out.println("Stop = S");
+                System.out.println("Reset = R");
+                System.out.println("Quit = Q");
+
+                System.out.print("Enter your choice: ");
+                response = in.nextLine().toUpperCase();
+
+                switch (response) {
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid choice!");
+                }
+
             }
 
         }
+        catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file is not supported! ;o");
+        }
         catch (FileNotFoundException e) {
-            System.out.println("Could not locate file location! :o");
+            System.out.println("Could not locate file! ;D");
+        }
+        catch (LineUnavailableException e) {
+            System.out.println(";A");
         }
         catch (IOException e) {
-            System.out.println("Could not write file! :|");
+            System.out.println("Something went wrong! ;p");
         }
-
-        in.close();
     }
 }
