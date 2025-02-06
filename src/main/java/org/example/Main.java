ba1b567a28781580f12467d7055f20c0ce986e1b
Ramazan <ramazan.abdullayev@bsu.edu.az>
2025-02-06 04:56:07 +0400

    Hangman Game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,60 +1,131 @@
 package org.example;
-import javax.sound.sampled.*;
-import javax.xml.stream.events.StartDocument;
-import java.io.File;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
 
-        String pathFile = "C:\\Users\\ramaz\\Downloads\\Billie Eilish - BIRDS OF A FEATHER (from Saturday Night Live, 2024).wav";
-        File file = new File(pathFile);
 
-        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-            Scanner in = new Scanner(System.in)) {
+        String filePath = "C:\\Users\\ramaz\\Desktop\\words.txt";
+        ArrayList<String> words = new ArrayList<>();
 
-            System.out.println("AAA");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
 
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioStream);
-            clip.start();
-
-            String response = "";
-
-            while (!response.equals("Q")) {
-                System.out.println("Play = P");
-                System.out.println("Stop = S");
-                System.out.println("Reset = R");
-                System.out.println("Quit = Q");
-
-                System.out.print("Enter your choice: ");
-                response = in.nextLine().toUpperCase();
-
-                switch (response) {
-                    case "P" -> clip.start();
-                    case "S" -> clip.stop();
-                    case "R" -> clip.setMicrosecondPosition(0);
-                    case "Q" -> clip.close();
-                    default -> System.out.println("Invalid choice!");
-                }
-
+            while ((line = reader.readLine()) != null) {
+                words.add(line.trim());
             }
-
-        }
-        catch (UnsupportedAudioFileException e) {
-            System.out.println("Audio file is not supported! ;o");
         }
         catch (FileNotFoundException e) {
-            System.out.println("Could not locate file! ;D");
-        }
-        catch (LineUnavailableException e) {
-            System.out.println(";A");
+            System.out.println("Could not locate file!");
         }
         catch (IOException e) {
-            System.out.println("Something went wrong! ;p");
+            System.out.println("Something went wrong...");
         }
+
+        String word = words.get(random.nextInt(words.size()));
+
+        int wrongGuesses = 0;
+
+        ArrayList<Character> wordState = new ArrayList<>();
+        for (int i = 0; i < word.length(); ++i) {
+            wordState.add('_');
+        }
+
+        System.out.println("************************");
+        System.out.println("Welcome to Hangman Game!");
+        System.out.println("************************");
+
+        while (wrongGuesses < 6) {
+            System.out.print(getHangmanArt(wrongGuesses));
+
+            System.out.print("Word: ");
+            for (char c : wordState) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+
+            System.out.print("Guess a letter: ");
+            char guess = in.next().toLowerCase().charAt(0);
+
+            if (word.indexOf(guess) >= 0) {
+                System.out.println("Correct");
+                for (int i = 0; i < word.length(); ++i) {
+                    if (word.charAt(i) == guess) {
+                        wordState.set(i, guess);
+                    }
+                }
+
+                if (!wordState.contains('_')) {
+                    System.out.println(getHangmanArt(wrongGuesses));
+                    System.out.println("You win!");
+                    System.out.println("The word is: " + word);
+                    break;
+                }
+            } else {
+                System.out.println("Wrong guess!");
+                wrongGuesses++;
+            }
+        }
+
+        if (wrongGuesses >= 6) {
+            System.out.print(getHangmanArt(wrongGuesses));
+            System.out.println("GAME OVER!");
+            System.out.println("The word is: " + word);
+        }
+
+
+        in.close();
+    }
+
+    static String getHangmanArt(int wrongGuesses) {
+        return switch (wrongGuesses) {
+            case 1 -> """
+                      o
+
+
+                    """;
+
+            case 2 -> """
+                      o
+                      |
+
+                    """;
+
+            case 3 -> """
+                      o
+                     /|
+
+                    """;
+
+            case 4 -> """
+                      o
+                     /|\\
+
+                    """;
+
+            case 5 -> """
+                      o
+                     /|\\
+                     /
+                    """;
+
+            case 6 -> """
+                      o
+                     /|\\
+                     / \\
+                    """;
+
+            default -> "";
+        };
     }
 }
