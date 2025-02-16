0acefc78b61b8997d2a80695a65190243300042a
Ramazan Abdullayev <r.abdullayev.dev@gmail.com>
2025-02-16 20:37:09 +0400

    Music Player

--- src/main/java/org/myspringapps/ClassicalMusic.java
+++ src/main/java/org/myspringapps/ClassicalMusic.java
@@ -1,9 +1,22 @@
 package org.myspringapps;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class ClassicalMusic implements Music {
+    private final List<String> songs = new ArrayList<>();
+
+    public ClassicalMusic() {
+        songs.add("Classical_1");
+        songs.add("Classical_2");
+        songs.add("Classical_3");
+    }
 
     @Override
-    public String getSong() {
-        return "Svoboda - Kipelov";
+    public List<String> getSongs() {
+        return songs;
     }
 }
